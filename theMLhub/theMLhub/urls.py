b78e2c9d6bc903da09394c32a208e1c69c17b6bf
abdelaziz-belhachmi <abdelaziz.belhachmi@etu.uae.ac.ma>
2024-12-10 15:22:30 +0100

    show profile details

--- theMLhub/theMLhub/urls.py
+++ theMLhub/theMLhub/urls.py
@@ -23,9 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),  # Root URL handled by my_app
 
-
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
